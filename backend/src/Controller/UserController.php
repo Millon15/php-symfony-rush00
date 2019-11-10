a24df680a34a39362175e10bd2ff0f84b9320edd
vbrazas <vbrazas@student.unit.ua>
2019-11-10 23:38:44 +0200

    userCreate done

--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -6,7 +6,7 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use App\Service\FileWriter;
-use App\Helper\ControllerHelper;
+use Symfony\Component\HttpClient\HttpClient;
 
 /**
  * Class UserController
@@ -20,6 +20,7 @@ class UserController
      *
      * @return Response
      * @throws \Exception
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
      */
     public function createUser(FileWriter $userFile, Request $request): Response
     {
@@ -29,31 +30,31 @@ class UserController
             throw new \RuntimeException("Can't retreive user name!");
         }
 
-        $id = md5(random_int(1, 10000) . random_int(1, 10000));
-        $user = ['id' => $id, 'userName' => $userName];
+        $client = HttpClient::create();
+        $userId = '';
+        $movies = [];
+        for ($i = 0; $i < 10; ++$i) {
+            if ($i === 0) {
+                $i = 1;
+            }
+            $page = random_int($i * 10000, 99999);
+            $userId .= $page;
+            $response = $client->request('GET', "http://www.omdbapi.com/?i=tt00{$page}&apikey=3fe767b6");
+            $movies[] = json_decode($response->getContent(), true);
+        }
 
-        $userFile->write($id, $user);
+        $createdAt = date('d-m-Y H:i:s');
+        $level = 0;
+        $user = [
+            'userId' => $userId,
+            'userName' => $userName,
+            'createdAt' => $createdAt,
+            'level' => $level,
+            'movies' => $movies
+        ];
 
-        return new Response($id);
+        $userFile->write($userId, $user);
+
+        return new Response($userId);
     }
-
-    // /**
-    //  * @param FileWriter $userFile
-    //  * @param Request    $request
-    //  *
-    //  * @return Response
-    //  * @throws \Exception
-    //  */
-    // public function saveGame(FileWriter $userFile, Request $request): Response
-    // {
-    //     $request_content = json_decode($request->getContent(), true);
-    //     $userName = $request_content['userId'];
-    //
-    //     $id = md5(random_int(1, 10000) . random_int(1, 10000));
-    //     $user = ['id' => $id, 'userName' => $userName];
-    //
-    //     $userFile->write($id, $user);
-    //
-    //     return ControllerHelper::buildResponse($id, false);
-    // }
 }
