2693b8dbb0bde1092f4c4dff1ca6db40964fcdc2
vbrazas <vbrazas@student.unit.ua>
2019-11-11 23:48:34 +0200

    Backend done

--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -3,11 +3,17 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
+use Exception;
+use RuntimeException;
 use App\Service\FileWriter;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
 
 /**
  * Class UserController
@@ -20,26 +26,26 @@ class UserController extends AbstractController
      * @param Request    $request
      *
      * @return Response
-     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
-     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
-     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
-     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
-     * @throws \Exception
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     * @throws Exception
      */
     public function createUser(FileWriter $userFile, Request $request): Response
     {
         $request_content = json_decode($request->getContent(), true);
         $userName = $request_content['userName'];
         if (!$userName) {
-            throw new \RuntimeException("Can't retreive user name!");
+            throw new RuntimeException("Can't retreive user name!");
         }
 
         $client = HttpClient::create();
         $userId = '';
         $movies = [];
         $const = 1000000;
-        for ($i = 1; $i <= 10; ++$i) {
-            $page = random_int((int)($const / (1000 * $i)), $const - 1);
+        for ($i = 0; $i < 10; ++$i) {
+            $page = random_int(10 ** $i % $const, $const - 1);
             $page = (string)($const + $page);
             $page[0] = 0;
             do {
@@ -49,8 +55,9 @@ class UserController extends AbstractController
             } while ($response['Response'] !== 'True');
 
             $movie = [
+                'id' => (int)$page,
                 'poster' => (($response['Poster'] ?? 'N/A') === 'N/A')
-                    ? 'https://eu.movieposter.com/posters/archive/main/13/MPW-6725'
+                    ? 'https://www.genesis.com/content/dam/genesis/us/images/chromecompare/not-available-icon-lrg.svg'
                     : $response['Poster'],
                 'name' => $response['Title'],
                 'plot' => $response['Plot'] ?? 'N/A',
@@ -72,13 +79,13 @@ class UserController extends AbstractController
         }
 
         $createdAt = date('d-m-Y H:i:s');
-        $level = 0;
+        $level = 1;
         $user = [
             'userId' => $userId,
             'userName' => $userName,
             'createdAt' => $createdAt,
             'level' => $level,
-            'movies' => $movies
+            'movies' => $movies,
         ];
 
         $userFile->write($userId, $user);
@@ -116,15 +123,88 @@ class UserController extends AbstractController
     public function userProgress(FileWriter $userFile, Request $request): Response
     {
         $userId = json_decode($request->getContent(), true)['userId']
-                           ?? $request->query->get('userId');
+                  ?? $request->query->get('userId');
         if (!$userId) {
-            throw new \RuntimeException("Can't retreive user id!");
+            throw new RuntimeException("Can't retreive user id!");
         }
         $user = $userFile->read()[$userId] ?? null;
         if (!$user) {
-            throw new \RuntimeException("Can't retreive user info for the user id: " . $userId);
+            throw new RuntimeException("Can't retreive user info by user id: " . $userId);
         }
 
         return $this->json(['movies' => $user['movies']]);
+    }
+
+    /**
+     * @param FileWriter $userFile
+     * @param Request    $request
+     *
+     * @return Response
+     * @throws Exception
+     */
+    public function fightCondinions(FileWriter $userFile, Request $request): Response
+    {
+        $userId = json_decode($request->getContent(), true)['userId']
+                  ?? $request->query->get('userId');
+        if (!$userId) {
+            throw new RuntimeException("Can't retreive user id!");
+        }
+        $user = $userFile->read()[$userId] ?? null;
+        if (!$user) {
+            throw new RuntimeException("Can't retreive user info by user id: " . $userId);
+        }
+        $movie = $user['movies'][random_int(0, 9)];
+
+        return $this->json([
+            'userInfo' => [
+                'hp' => $user['level'] * 3,
+                'damage' => $user['level'] * 2,
+            ],
+            'enemy' => [
+                'id' => $movie['id'],
+                'poster' => $movie['poster'],
+                'hp' => $user['level'] * 4,
+                'damage' => $user['level'] * 1,
+            ],
+        ]);
+    }
+
+    /**
+     * @param FileWriter $userFile
+     * @param Request    $request
+     *
+     * @return Response
+     */
+    public function gameOver(FileWriter $userFile, Request $request): Response
+    {
+        $j = json_decode($request->getContent(), true);
+        if (!$j['userId']) {
+            throw new RuntimeException("Can't retreive user id!");
+        }
+        if (!$j['movieId']) {
+            throw new RuntimeException("Can't retreive movie id!");
+        }
+
+        $user = $userFile->read()[$j['userId']] ?? null;
+        if (!$user) {
+            throw new RuntimeException("Can't retreive user info by user id: " . $j['userId']);
+        }
+
+        foreach ($user['movies'] as &$mov) {
+            if ($mov['id'] == $j['movieId']) {
+                $movie = &$mov;
+            }
+        }
+        unset($mov);
+        if (!isset($movie)) {
+            throw new RuntimeException("Can't retreive movie info by movie id: " . $j['movieId']);
+        }
+
+        $user['level'] += 1;
+        $movie['isDefeated'] = true;
+
+        $userFile->update($j['userId'], $user);
+
+        return new Response();
     }
 }
