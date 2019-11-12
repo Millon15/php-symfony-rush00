e1353aa7fbc191b0da15e11c11251e9893bafd35
Ira Novykova <inovykova@axondevgroup.com>
2019-11-12 11:29:57 +0200

    victory sign on poster

--- frontend/src/pages/Galery.jsx
+++ frontend/src/pages/Galery.jsx
@@ -9,6 +9,7 @@ import Button from '@material-ui/core/Button';
 import axios from 'axios';
 
 import requestRoutes from '../config/requestRoutes';
+import VictoryIcon from '../assets/VictoryIcon';
 
 const mock = [{poster: 'https://eu.movieposter.com/posters/archive/main/22/A70-11370', name: 'Gladiator', isDefeated: false, id: '12456'}, {id: '11', poster: 'https://eu.movieposter.com/posters/archive/main/38/MPW-19355', name: 'Forest Gump', isDefeated: true}, {id: '222', poster: 'https://eu.movieposter.com/posters/archive/main/38/MPW-19355', name: 'Forest Gump', isDefeated: true}, {id: '33', poster: 'https://eu.movieposter.com/posters/archive/main/38/MPW-19355', name: 'Forest Gump', isDefeated: true}, {id: '44', poster: 'https://eu.movieposter.com/posters/archive/main/38/MPW-19355', name: 'Forest Gump', isDefeated: false}, {id: '555', poster: 'https://eu.movieposter.com/posters/archive/main/38/MPW-19355', name: 'Forest Gump', isDefeated: true}, {id: '66', poster: 'https://eu.movieposter.com/posters/archive/main/38/MPW-19355', name: 'Forest Gump', isDefeated: false}, {id: '77', poster: 'https://eu.movieposter.com/posters/archive/main/5/A70-2978', name: 'Good felows', isDefeated: true}, {id: '88', poster: 'https://eu.movieposter.com/posters/archive/main/7/A70-3860', name: '007', isDefeated: false}, {id: '899', poster: 'https://eu.movieposter.com/posters/archive/main/13/MPW-6725', name: 'Sound of music', isDefeated: false}];
 
@@ -55,7 +56,14 @@ const Galery = ({ history }) => {
             {info.map(movie => (
                 <div key={movie.id} className="moviemonsterItem">
                     <Paper className={classes.root}>
-                        <img src={movie.poster} alt={movie.name} className={`moviemonsterPic ${movie.isDefeated ? '' : 'notDefeated'}`} />
+                        <div className="moviemonsterHolder">
+                            <img src={movie.poster} alt={movie.name} className={`moviemonsterPic ${movie.isDefeated ? '' : 'notDefeated'}`} />
+                            {movie.isDefeated && (
+                                <span className="victory">
+                                    <VictoryIcon />
+                                </span>
+                            )}
+                        </div>
                         <Typography variant="h5" component="h3" className={classes.title}>
                             {movie.name}
                         </Typography>
